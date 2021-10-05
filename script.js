215638055aa6740fba05b705be2520e9b55bd508
Evgenio89 <evgenio891@gmail.com>
2021-10-05 23:37:29 +0300

    freeCodeCamp

--- script.js
+++ script.js
@@ -9,6 +9,24 @@ var testObj = {
 };
 
 // Only change code below this line
+//Change the code so that all variables are declared using let or const.
+// Use let when you want the variable to change, and const when you want the variable to remain constant.
+// Also, rename variables declared with const to conform to common practices,
+// meaning constants should be in all caps.
 
 var hatValue = testObj.hat;      // Change this line
 var shirtValue = testObj.shirt;    // Change this line
+
+function printManyTimes(str) {
+
+    // Only change code below this line
+
+    const SENTENCE = str + " is cool!";
+    for (let i = 0; i < str.length; i+=2) {
+        console.log(SENTENCE);
+    }
+
+    // Only change code above this line
+
+}
+printManyTimes("freeCodeCamp");
