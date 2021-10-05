9f3d07465f4b0e0cb834f273099ce8938653f005
Evgenio89 <evgenio891@gmail.com>
2021-10-05 23:27:25 +0300

    codeWars

--- codeWars.js
+++ codeWars.js
@@ -12,3 +12,16 @@ function readOut(words) {
     return words.map(first_letter).join('');
 
 }
+//The following was a question that I received during a technical interview for an entry level
+// software developer position. I thought I'd post it here so that everyone could give it a go:
+
+//You are given an unsorted array containing all the integers from 0 to 100 inclusively.
+// However, one number is missing. Write a function to find and return this number.
+// What are the time and space complexities of your solution?
+
+   // ALGORITHMS
+function missingNo(arr) {
+    const sum = ((99 + 1) * (99 + 2)) / 2;
+    const arrSum = (arr) => arr.reduce((a, b) => a + b, 0);
+    return sum - arrSum(arr);
+}
