5f3e00a03e7890a72fe8dc27539b8c9eefd239cc
Evgenio89 <evgenio891@gmail.com>
2021-10-05 22:38:50 +0300

    question 82

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-\\ задание 24
+// задание 24
 var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+// задание 82
+// Setup
+var testObj = {
+    hat: "ballcap",
+    shirt: "jersey",
+    "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
